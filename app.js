056e381b1d501634460e48545148906016b3ba9e
RMCM12 <97398046+RMCM12@users.noreply.github.com>
2024-07-12 23:22:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -21,31 +21,14 @@ mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
 });
 
-// Define models (example)
-const User = mongoose.model('User', new mongoose.Schema({
-  username: String,
-  password: String,
-  avatar: String,
-  description: String,
-}));
-
 // Routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+const reviewRouter = require('./routes/review');
 
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'register.html'));
-});
-
-app.post('/register', async (req, res) => {
-  const { username, password, avatar, description } = req.body;
-  const user = new User({ username, password, avatar, description });
-  await user.save();
-  res.redirect('/');
-});
-
-// Add other routes similarly
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use('/review', reviewRouter);
 
 // Start server
 app.listen(port, () => {
