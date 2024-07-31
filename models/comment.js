1926fc3d57f5950ef094d4f9db59e7ac943efc22
RMCM12 <97398046+RMCM12@users.noreply.github.com>
2024-08-01 05:34:20 +0800

    Delete models directory

--- models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    review: { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
-});
-
-module.exports = mongoose.model('Comment', commentSchema);
